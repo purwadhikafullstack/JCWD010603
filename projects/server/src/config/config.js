8f0ad0082563b527d877b7503c1779676d9bd5f4
Bryan Karta <65345089+Servond@users.noreply.github.com>
2023-05-12 09:40:55 +0700

    Merge v3 (#11)
    * update
    
    * added status transactions
    
    * report page done
    
    * update
    
    * backend for cancel and send order
    
    update code utk cart & product list buat munculin gambar sesuai path
    
    * backend send & cancel
    
    * update backend cancel and send
    
    * update
    
    * BranchAdminSendCancelIsDone
    
    * update tgl 9
    
    * edit-bug
    
    * fix bug 2 update
    
    * update 2
    
    * update user order
    
    * Sync repo 01 (#53)
    
    * merge v1 (#5)
    
    * ci: update port dan projectname
    
    * feat: add folder routes backend
    
    * update
    
    * backend setup
    
    package instalation
    db
    migrate
    assoc
    
    * create route
    
    * Update index.js
    
    * update redux
    
    * Admin Login/ Dashboard/ Setting
    
    8 March 2023
    
    * update frontend
    
    * registration
    
    backend
    
    * create admin & branch
    
    * protected page
    
    * update tgl 13
    
    * update tgl 14
    
    * update route.js
    
    * update
    
    * update sore tgl 14
    
    * update tgl 14 malam
    
    * update
    
    * Update .gitignore
    
    * update tgl 16 sore
    
    * update api rajaongkir
    
    fetching api rajaongkir
    
    * Update .gitignore
    
    * change
    
    * update
    
    * update tgl 17
    
    * product category
    
    * uodate tgl 22
    
    * perbaiki error protected page
    
    * update-pagination
    
    * new update
    
    still on progress
    
    * adminAuthorization
    
    * protectedPage(setting_complete)
    
    * authorization complete
    
    * final fix
    
    * push
    
    * Merge branch 'development' of https://github.com/ridhozhr10/JCWD010603 into development
    
    * update tgl 28
    
    * new setup sequelize
    
    * see-dashboard-page
    
    * createBranch pakai API raja ongkir
    
    * detail product tgl 30
    
    * Cart_page
    
    Kurang pagination
    
    * update list-product
    
    * update-list-produk
    
    * product & stock update
    
    * CART_VOUCHER(FE)
    
    * add product to cart - complete
    
    * update-list-produk
    
    * can create new order
    
    * task crud product
    
    * update tgl 7
    
    * create voucher FE & BE
    
    * fix bug
    
    * uploadpayment
    
    * cart and voucher update
    
    * detail-create-order
    
    * update
    
    * landingPage_done
    
    ---------
    
    Co-authored-by: ridhozhr10 <azharridho42@gmail.com>
    Co-authored-by: randytjioe <randytjioe95@gmail.com>
    Co-authored-by: IANOONE <86506939+IANOONE@users.noreply.github.com>
    Co-authored-by: randytjioe <34883702+randytjioe@users.noreply.github.com>
    Co-authored-by: Erwin Gunawan <erwin.dg8ts@gmail.com>
    Co-authored-by: Erwin Gunawan <117703260+erwindggr@users.noreply.github.com>
    
    * Update index.js
    
    fix path to env
    
    * remove components
    
    list_address
    sidebar_product
    
    ---------
    
    Co-authored-by: ridhozhr10 <azharridho42@gmail.com>
    Co-authored-by: randytjioe <randytjioe95@gmail.com>
    Co-authored-by: IANOONE <86506939+IANOONE@users.noreply.github.com>
    Co-authored-by: randytjioe <34883702+randytjioe@users.noreply.github.com>
    Co-authored-by: Erwin Gunawan <erwin.dg8ts@gmail.com>
    Co-authored-by: Erwin Gunawan <117703260+erwindggr@users.noreply.github.com>
    
    * update
    
    * fix bug
    
    * nambah api
    
    * tambahin return + transaction di controller
    
    * bugFixing#1Erwin
    
    * #2-bugFixes-Erwin
    
    * nambah api
    
    * bug fix pagination
    
    * fix code
    
    * munculin gambar produk di report dashboard
    
    * edit bug 3
    
    * edit bug 3 update
    
    ---------
    
    Co-authored-by: Erwin Gunawan <erwin.dg8ts@gmail.com>
    Co-authored-by: IANOONE <86506939+IANOONE@users.noreply.github.com>
    Co-authored-by: randytjioe <34883702+randytjioe@users.noreply.github.com>
    Co-authored-by: randytjioe <randytjioe95@gmail.com>
    Co-authored-by: Erwin Gunawan <117703260+erwindggr@users.noreply.github.com>
    Co-authored-by: ridhozhr10 <azharridho42@gmail.com>

--- projects/server/src/config/config.js
+++ projects/server/src/config/config.js
@@ -1,10 +1,10 @@
 module.exports = {
   development: {
-    username: process.env.user, 
+    username: process.env.user,
     password: process.env.pass,
     database: process.env.database,
     host: process.env.host,
-    dialect: process.env.db_dialect,
+    dialect: "mysql",
     timezone: '+07:00'
   }
 
@@ -22,5 +22,4 @@ module.exports = {
   //   host= "127.0.0.1",
   //   dialect= "mysql"
   // }
-
-}
+};
